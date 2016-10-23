bc695e732f06db46fa0a3e420c7d4bc40a9f6167
TheCrafter <dimitrisvlh@gmail.com>
2016-10-23 22:54:14 +0300

    Code cleanup in main

--- src/main.c
+++ src/main.c
@@ -14,11 +14,6 @@
 #define MAX_VERTEX_BUFFER 512 * 1024
 #define MAX_ELEMENT_BUFFER 128 * 1024
 
-#define UNUSED(a) (void)a
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define MAX(a,b) ((a) < (b) ? (b) : (a))
-#define LEN(a) (sizeof(a)/sizeof(a)[0])
-
 void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %d %s\n", error, description);
@@ -34,6 +29,25 @@ void key_callback(struct window* window, int key, int scancode, int action, int 
         window_close(window);
 }
 
+void render(struct window* window, struct nk_color* background)
+{
+    float bg[4];
+    int width, height;
+
+    nk_color_fv(bg, *background);
+    window_get_size(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(bg[0], bg[1], bg[2], bg[3]);
+    /* IMPORTANT: `nk_glfw_render` modifies some global OpenGL state
+     * with blending, scissor, face culling, depth test and viewport and
+     * defaults everything back into a default state.
+     * Make sure to either a.) save and restore or b.) reset your own state after
+     * rendering the UI.
+     */
+    nk_glfw3_render(NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+}
+
 int main()
 {
     // Create window
@@ -41,17 +55,16 @@ int main()
     if (window == NULL)
         return -1;
 
-    window_set_error_callback(window, error_callback);
-    window_set_key_callback(window, key_callback);
-
     /* Get GLFW handle */
     GLFWwindow* glfw_handle = window_get_glfw_handle(window);
 
+    /* Set callbacks */
+    window_set_error_callback(window, error_callback);
+    window_set_key_callback(window, key_callback);
+
     /* Platform */
-    //static GLFWwindow *win;
     int width = 0, height = 0;
     struct nk_context *ctx;
-    struct nk_color background;
 
     /* GLFW */
     window_get_size(window, &width, &height);
@@ -59,20 +72,22 @@ int main()
     /* OpenGL */
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    /* Init nuklear with GLFW */
     ctx = nk_glfw3_init(glfw_handle, NK_GLFW3_INSTALL_CALLBACKS);
+
     /* Load Fonts: if none of these are loaded a default font will be used  */
-    /* Load Cursor: if you uncomment cursor loading please hide the cursor */
-    {struct nk_font_atlas *atlas;
-    nk_glfw3_font_stash_begin(&atlas);
-    /*struct nk_font *droid = nk_font_atlas_add_from_file(atlas, "../../../extra_font/DroidSans.ttf", 14, 0);*/
-    struct nk_font *roboto = nk_font_atlas_add_from_file(atlas, "res/Font/Roboto-Regular.ttf", 16, 0);
-    /*struct nk_font *future = nk_font_atlas_add_from_file(atlas, "../../../extra_font/kenvector_future_thin.ttf", 13, 0);*/
-    /*struct nk_font *clean = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyClean.ttf", 12, 0);*/
-    /*struct nk_font *tiny = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyTiny.ttf", 10, 0);*/
-    /*struct nk_font *cousine = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Cousine-Regular.ttf", 13, 0);*/
-    nk_glfw3_font_stash_end();
-    /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
-    nk_style_set_font(ctx, &roboto->handle);}
+    {
+        struct nk_font_atlas *atlas;
+        nk_glfw3_font_stash_begin(&atlas);
+
+        struct nk_font *roboto = nk_font_atlas_add_from_file(atlas, "res/Font/Roboto-Regular.ttf", 16, 0);
+        nk_glfw3_font_stash_end();
+
+        /* Load Cursor: if you uncomment cursor loading please hide the cursor */
+        /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
+
+        nk_style_set_font(ctx, &roboto->handle);
+    }
 
     /* style.c */
     /*set_style(ctx, THEME_WHITE);*/
@@ -80,70 +95,57 @@ int main()
     /*set_style(ctx, THEME_BLUE);*/
     /*set_style(ctx, THEME_DARK);*/
 
-    background = nk_rgb(28,48,62);
+    struct nk_color background = nk_rgb(28,48,62);
     while (!window_should_close(window))
     {
         /* Input */
-        glfwPollEvents();
+        window_update(window);
         nk_glfw3_new_frame();
 
         /* GUI */
-        {struct nk_panel layout;
-        if (nk_begin(ctx, &layout, "Demo", nk_rect(50, 50, 230, 250),
-            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
         {
-            enum {EASY, HARD};
-            static int op = EASY;
-            static int property = 20;
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                fprintf(stdout, "button pressed\n");
+            struct nk_panel layout;
+            if (nk_begin(ctx, &layout, "Demo", nk_rect(50, 50, 230, 250),
+                NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+                NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+            {
+                enum {EASY, HARD};
+                static int op = EASY;
+                static int property = 20;
+                nk_layout_row_static(ctx, 30, 80, 1);
+                if (nk_button_label(ctx, "button"))
+                    fprintf(stdout, "button pressed\n");
 
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+                nk_layout_row_dynamic(ctx, 30, 2);
+                if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+                if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
 
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-            {struct nk_panel combo;
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, &combo, background, nk_vec2(nk_widget_width(ctx),400))) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                background = nk_color_picker(ctx, background, NK_RGBA);
                 nk_layout_row_dynamic(ctx, 25, 1);
-                background.r = (nk_byte)nk_propertyi(ctx, "#R:", 0, background.r, 255, 1,1);
-                background.g = (nk_byte)nk_propertyi(ctx, "#G:", 0, background.g, 255, 1,1);
-                background.b = (nk_byte)nk_propertyi(ctx, "#B:", 0, background.b, 255, 1,1);
-                background.a = (nk_byte)nk_propertyi(ctx, "#A:", 0, background.a, 255, 1,1);
-                nk_combo_end(ctx);
-            }}
+                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+                {struct nk_panel combo;
+                nk_layout_row_dynamic(ctx, 20, 1);
+                nk_label(ctx, "background:", NK_TEXT_LEFT);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                if (nk_combo_begin_color(ctx, &combo, background, nk_vec2(nk_widget_width(ctx),400))) {
+                    nk_layout_row_dynamic(ctx, 120, 1);
+                    background = nk_color_picker(ctx, background, NK_RGBA);
+                    nk_layout_row_dynamic(ctx, 25, 1);
+                    background.r = (nk_byte)nk_propertyi(ctx, "#R:", 0, background.r, 255, 1,1);
+                    background.g = (nk_byte)nk_propertyi(ctx, "#G:", 0, background.g, 255, 1,1);
+                    background.b = (nk_byte)nk_propertyi(ctx, "#B:", 0, background.b, 255, 1,1);
+                    background.a = (nk_byte)nk_propertyi(ctx, "#A:", 0, background.a, 255, 1,1);
+                    nk_combo_end(ctx);
+                }}
+            }
+            nk_end(ctx);
         }
-        nk_end(ctx);}
 
-        /* -------------- EXAMPLES ---------------- */
-        /*calculator(ctx);*/
-        /*overview(ctx);*/
-        /*node_editor(ctx);*/
-        /* ----------------------------------------- */
+        /* Render */
+        render(window, &background);
 
-        /* Draw */
-        {float bg[4];
-        nk_color_fv(bg, background);
-        window_get_size(window, &width, &height);
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(bg[0], bg[1], bg[2], bg[3]);
-        /* IMPORTANT: `nk_glfw_render` modifies some global OpenGL state
-         * with blending, scissor, face culling, depth test and viewport and
-         * defaults everything back into a default state.
-         * Make sure to either a.) save and restore or b.) reset your own state after
-         * rendering the UI. */
-        nk_glfw3_render(NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
-        window_swap_buffers(window);}
+        /* Swap buffers */
+        window_swap_buffers(window);
     }
     nk_glfw3_shutdown();
     glfwTerminate();
