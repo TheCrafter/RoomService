b0266487e60b72fcceb692ee0274a658362452c9
TheCrafter <dimitrisvlh@gmail.com>
2016-12-21 19:12:28 +0200

    Added macu to external deps

--- config.mk
+++ config.mk
@@ -2,11 +2,12 @@ PRJTYPE = Executable
 ifeq ($(TARGET_OS), Windows_NT)
 	DEFINES += _CRT_SECURE_NO_WARNINGS _CRT_SECURE_NO_DEPRECATE
 endif
-EXTDEPS = macu::0.0.2dev
 
-LIBS = nuklear glfw glad
+LIBS = nuklear macu glfw glad
 ifeq ($(TARGET_OS), Windows_NT)
 	LIBS += glu32 opengl32 gdi32 ole32 user32 shell32
 else
     	LIBS += GL X11 Xrandr Xinerama Xcursor pthread dl
 endif
+
+EXTDEPS = macu::0.0.2dev
