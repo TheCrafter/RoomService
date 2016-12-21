0270be0ad21721f1fe12f60708c354ebc2818214
TheCrafter <dimitrisvlh@gmail.com>
2016-12-21 18:23:26 +0200

    Upgraded build system

--- config.mk
+++ config.mk
@@ -1,6 +1,11 @@
 PRJTYPE = Executable
+ifeq ($(TARGET_OS), Windows_NT)
+	DEFINES += _CRT_SECURE_NO_WARNINGS _CRT_SECURE_NO_DEPRECATE
+endif
+EXTDEPS = macu::0.0.2dev
+
 LIBS = nuklear glfw glad
-ifeq ($(OS), Windows_NT)
+ifeq ($(TARGET_OS), Windows_NT)
 	LIBS += glu32 opengl32 gdi32 ole32 user32 shell32
 else
     	LIBS += GL X11 Xrandr Xinerama Xcursor pthread dl
