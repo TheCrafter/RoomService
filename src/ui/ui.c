8c3540d2ee875343ca982c758f3918661cdf68b1
TheCrafter <dimitrisvlh@gmail.com>
2016-12-25 13:49:15 +0200

    Removed test panel

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -61,58 +61,6 @@ void ui_destroy(struct ui_context* ctx)
     ui_widget_log_destroy(&ctx->info_view);
 }
 
-static void ui_extra_layout_render(struct ui_context* ui_ctx)
-{
-    struct nk_context* ctx = ui_ctx->nk_ctx;
-    struct nk_color background = nk_rgb_fv(ui_ctx->bg);
-    struct nk_panel layout;
-
-    if (nk_begin(ctx, &layout, "Demo", nk_rect(300, 300, 230, 250),
-                NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-                NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
-    {
-        enum {EASY, HARD};
-        static int op = EASY;
-        static int property = 20;
-        nk_layout_row_static(ctx, 30, 80, 1);
-        if (nk_button_label(ctx, "Button"))
-        {
-            ui_widget_log_append(&ui_ctx->log_view, "Button pressed");
-            ui_widget_log_append(&ui_ctx->info_view, "Pressed button");
-        }
-
-        nk_layout_row_dynamic(ctx, 30, 2);
-        if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-        if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-
-        nk_layout_row_dynamic(ctx, 25, 1);
-        nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-        /* Background control widget */
-        {
-            struct nk_panel combo;
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, &combo, background, nk_vec2(nk_widget_width(ctx),400)))
-            {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                background = nk_color_picker(ctx, background, NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                background.r = (nk_byte)nk_propertyi(ctx, "#R:", 0, background.r, 255, 1,1);
-                background.g = (nk_byte)nk_propertyi(ctx, "#G:", 0, background.g, 255, 1,1);
-                background.b = (nk_byte)nk_propertyi(ctx, "#B:", 0, background.b, 255, 1,1);
-                background.a = (nk_byte)nk_propertyi(ctx, "#A:", 0, background.a, 255, 1,1);
-                nk_combo_end(ctx);
-            }
-        }
-    }
-    nk_end(ctx);
-
-    /* Save background color */
-    nk_color_fv(ui_ctx->bg, background);
-}
-
 static void ui_view_render(struct ui_context* ctx, struct ui_view* view)
 {
     view->render_cb(ctx->nk_ctx, view);
@@ -126,9 +74,6 @@ void ui_render(struct ui_context* ctx)
     /* Render widgets */
     ui_view_render(ctx, &ctx->log_view);
     ui_view_render(ctx, &ctx->info_view);
-
-    /* Render extra layout (non-widgets) */
-    ui_extra_layout_render(ctx);
 
     /* IMPORTANT: `nk_glfw_render` modifies some global OpenGL state
      * with blending, scissor, face culling, depth test and viewport and
